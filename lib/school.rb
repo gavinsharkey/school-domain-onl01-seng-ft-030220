fdc773b0d0b7e3f70e67b8d46934db9853bd4bc5
Gavin Sharkey <gavinsharkey2016@gmail.com>
2020-03-05 20:03:38 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,7 +3,15 @@ class School
 
   def initialize(title)
     @title = title
-
     @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster[grade]
+      @roster[grade] << name
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+    end
   end
 end
